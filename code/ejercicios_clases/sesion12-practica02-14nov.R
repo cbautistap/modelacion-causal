4ce81208f2bad0effe722d43af90c479f281d0cf
Carlos Bautista <cbautistap92@gmail.com>
2022-11-14 18:28:53 -0600

    practica 2

---
+++ code/ejercicios_clases/sesion12-practica02-14nov.R
@@ -0,0 +1,16 @@
+# Sesión 12. 
+# Práctica 2. Variables instrumentales
+
+library(AER)
+library(haven)
+library(tidyverse)
+
+# 1. Cargar datos
+data("CollegeDistance")
+attach(CollegeDistance)
+View(CollegeDistance)
+
+# 2. Obtener descripción general del conjunto de datos
+summary(CollegeDistance)
+
+# obtene
