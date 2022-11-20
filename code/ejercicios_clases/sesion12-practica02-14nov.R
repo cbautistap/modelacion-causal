cf0e71a41775341f906c115a4c3e53ff9b842d90
Carlos Bautista <cbautistap92@gmail.com>
2022-11-20 17:58:50 -0600

    practica 2 puntos 1 a 3

--- code/ejercicios_clases/sesion12-practica02-14nov.R
+++ code/ejercicios_clases/sesion12-practica02-14nov.R
@@ -11,11 +11,27 @@ attach(CollegeDistance)
 View(CollegeDistance)
 
 # 2. Obtener descripción general del conjunto de datos
+# Transformaciones
+CollegeDistance <- CollegeDistance %>% 
+  mutate(hispanic = case_when(ethnicity=="hispanic" ~ 1, TRUE ~ 0),
+         afam = case_when(ethnicity=="afam" ~ 1, TRUE ~ 0),
+         female = case_when(gender=="female" ~ 1, TRUE ~ 0),
+         urban = case_when(urban=="yes" ~1, TRUE~ 0),
+         lwage = log(wage),
+         leduc = log(education))
+
+attach(CollegeDistance)
+
 summary(CollegeDistance)
+
 # Histograma para visualizar distancia
 hist(distance)
 
 
 # 3. Estime dos regresiones no confiables
 # 3.1 
-lwage 
+reg1_nc <- lm(lwage ~ leduc) 
+summary(reg1_nc)
+
+reg2_nc <- lm(lwage ~ leduc + hispanic + afam + female + urban)
+summary(reg2_nc)
